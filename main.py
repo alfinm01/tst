baafe99f759997c4d480ff05ef631e7f9f20498d
alfinm01 <arrestral@gmail.com>
2019-11-29 14:50:44 +0700

    Change route /nation to /wiki and add route /wiki/raw

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def auth_check(token):
 #
 @app.route('/')
 def documentation():
-	return redirect('https://app.swaggerhub.com/apis-docs/alfinm01/TST_NationRegionAPI/1.0.0', code = 303)
+	return redirect('https://app.swaggerhub.com/apis-docs/alfinm01/TST_WikiRegionAPI/1.0.0', code = 303)
 
 #
 # Get access token
@@ -74,37 +74,37 @@ def get_token(nim):
 		raise e
 
 #
-# Get desired nation data from Wikipedia
+# Get desired region data from Wikipedia
 #
-@app.route('/nation')
-def nation():
+@app.route('/wiki')
+def wiki():
 	try:
 		# Authentication check
 		#token = request.headers.get('token')
 		#auth_check(token)
 
 		# Get Wikipedia content
-		_nation = request.args.get('name')
-		if _nation is None:
-			return Response('Must include nation query', 400)
+		_wiki = request.args.get('name')
+		if _wiki is None:
+			return Response('Must include wiki query', 400)
 		_language = request.args.get('language')
 		if _language:
-			_result = requests.get('https://' + _language + '.' + wikipedia + _nation)
+			_result = requests.get('https://' + _language + '.' + wikipedia + _wiki)
 		else:
-			_result = requests.get('https://' + default_language + '.' + wikipedia + _nation)
+			_result = requests.get('https://' + default_language + '.' + wikipedia + _wiki)
 		_soup = BeautifulSoup(_result.text, 'html.parser')
 
 		# Wiki not found
 		_not_found = _soup.find('div', attrs = {'class' : 'noarticletext'})
 		if _not_found:
-			return Response('Nation not found', 404)
+			return Response('Wiki not found', 404)
 		
 		# Parsing from HTML to JSON
 		_table = _soup.find('table', attrs = {'class' : 'infobox'})
-		
-		# Wrong nation query
+
+		# Wrong wiki query
 		if _table == None:
-			return Response('Nation query is wrong', 404)
+			return Response('Wiki query is wrong', 404)
 
 		# Iterate every tr
 		_tbody = _table.find('tbody')
@@ -118,13 +118,13 @@ def nation():
 
 			# Parsing dictionary key
 			if _tr.find('a'):
-				_key = _tr.find('a').string
+				_key = _tr.find('a').text
 			else:
 				if _tr.find('b'):
-					_key = _tr.find('b').string
+					_key = _tr.find('b').text
 				else:
 					if _tr.find('div'):
-						_key = _tr.find('div').string
+						_key = _tr.find('div').text
 					else:
 						_key = 'uncrawled object ' + str(_counter)
 
@@ -143,9 +143,56 @@ def nation():
 	except Exception as e:
 		raise e
 
+#
+# Get desired region data from Wikipedia in form of raw data
+#
+@app.route('/wiki/raw')
+def wiki_raw():
+	try:
+		# Authentication check
+		#token = request.headers.get('token')
+		#auth_check(token)
 
-# Route below is using direct method (without database storage)
-# Possibility of change in the future
+		# Get Wikipedia content
+		_wiki = request.args.get('name')
+		if _wiki is None:
+			return Response('Must include wiki query', 400)
+		_language = request.args.get('language')
+		if _language:
+			_result = requests.get('https://' + _language + '.' + wikipedia + _wiki)
+		else:
+			_result = requests.get('https://' + default_language + '.' + wikipedia + _wiki)
+		_soup = BeautifulSoup(_result.text, 'html.parser')
+
+		# Wiki not found
+		_not_found = _soup.find('div', attrs = {'class' : 'noarticletext'})
+		if _not_found:
+			return Response('Wiki not found', 404)
+		
+		# Find table
+		_table = _soup.find('table', attrs = {'class' : 'infobox'})
+
+		# Wrong wiki query
+		if _table == None:
+			return Response('Wiki query is wrong', 404)
+
+		# Find first paragraph
+		_div = _soup.find('div', attrs = {'class' : 'mw-parser-output'})
+		_p = _soup.find('p')
+
+		# Wrong wiki query
+		if _p == None:
+			return Response('Wiki query is wrong', 404)
+
+		# Iterate every tr
+		_data = {}
+		_data['table'] = str(_table)
+		_data['paragraph'] = str(_p)
+
+		return jsonify(_data)
+
+	except Exception as e:
+		raise e
 
 #
 # Get Indonesian provinces data
