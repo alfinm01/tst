f2a9a9e065c63540f508323b0a9116db0cde3e42
alfinm01 <arrestral@gmail.com>
2019-11-29 15:49:32 +0700

    Change threaded to true

--- main.py
+++ main.py
@@ -269,6 +269,6 @@ def village(district_id):
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(threaded = True, port = 5000)
     #app.run(ssl_context = ('cert.pem', 'key.pem'))
     #app.run(debug = True)
